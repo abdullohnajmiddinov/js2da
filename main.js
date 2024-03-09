e0983c22ae97dc58d5337750016c94c2a3dccfa6
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-03-09 10:07:55 +0300

    salom

---
+++ main.js
@@ -0,0 +1,8 @@
+let a = prompt('String typedagi malumotni kriting!')
+let b = prompt('Number typedagi malumotni kriting!')
+let d = prompt('Boolean typedagi malumotni kriting!')
+alert('hamma malumotlarni togri kiritkaningizga ishonchingiz komilmi?')
+alert(`Siz kiritgan ${a} sozi String typega tegishli\nSiz kiritgan ${b} sozi Number typega tegishli\nSiz kiritgan ${d} sozi Boolean typega tegishli`)
+console.log('String: '+a);
+console.log('Number: '+b);
+console.log('Boolen: '+d);
